c49bd45c5fbf170ce2ad7e83684b08d2e879aff2
jay-alx <126336909+younis-alx@users.noreply.github.com>
2024-01-10 10:28:33 +0000

    optimized to O(n*m)

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Checks if all locks has corresponding keys
+@boxes: list of lists
+
+Returns: Boolean
+ O(n+m)
+"""
+
+
+def canUnlockAll(boxes):
+    visited = set()
+    queue = [0]
+    while queue:
+        box = queue.pop(0)
+        visited.add(box)
+        for key in boxes[box]:
+            if key not in visited:
+                queue.append(key)
+    return len(visited) == len(boxes)
