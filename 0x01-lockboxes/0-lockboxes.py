c39a24a6f7d0b2c2619634df79eb7b59944f8955
jay-alx <126336909+younis-alx@users.noreply.github.com>
2024-01-10 10:40:48 +0000

    edge cases

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,10 +13,14 @@ def canUnlockAll(boxes):
     """
     returns boolean whether all boxes can be opened
     """
+    if not boxes:
+        return True
     visited = set()
     queue = [0]
     while queue:
         box = queue.pop(0)
+        if box in visited:
+            continue
         visited.add(box)
         for key in boxes[box]:
             if key not in visited:
