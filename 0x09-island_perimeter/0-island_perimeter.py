b936156862fb770f143d807f0d767c910885ce94
jay-alx <126336909+younis-alx@users.noreply.github.com>
2024-03-06 08:08:10 +0000

    update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,17 +11,35 @@ def island_perimeter(grid):
     Return:
         the perimeter of the island
     """
+    perimeter = 0
 
-    p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if (grid[i][j] == 1):
-                if (i <= 0 or grid[i - 1][j] == 0):
-                    p += 1
-                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
-                    p += 1
-                if (j <= 0 or grid[i][j - 1] == 0):
-                    p += 1
-                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
-                    p += 1
-    return p
+            if grid[i][j] == 1:
+                perimeter += calculate_cell_perimeter(grid, i, j)
+
+    return perimeter
+
+
+def calculate_cell_perimeter(grid, i, j):
+    """
+    Calculates the perimeter of a single cell in the island
+    Args:
+        grid: 2d list of integers containing 0(water) or 1(land)
+        i: row index of the cell
+        j: column index of the cell
+    Return:
+        the perimeter of the cell
+    """
+    cell_perimeter = 0
+
+    if i <= 0 or grid[i - 1][j] == 0:
+        cell_perimeter += 1
+    if i >= len(grid) - 1 or grid[i + 1][j] == 0:
+        cell_perimeter += 1
+    if j <= 0 or grid[i][j - 1] == 0:
+        cell_perimeter += 1
+    if j >= len(grid[i]) - 1 or grid[i][j + 1] == 0:
+        cell_perimeter += 1
+
+    return cell_perimeter
